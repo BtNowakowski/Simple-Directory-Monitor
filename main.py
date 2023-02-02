94b48921c66b6e610cfec3d7efc465e9b2d0a83c
BtNowakowski <107316656+BtNowakowski@users.noreply.github.com>
2023-02-02 15:51:04 +0100

    important

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import os
 from idlelib.tooltip import Hovertip
 from Watcher import Watcher
 
-EXAMPLE_DEST = {"monitored_directory":"C:\\Users\\[WINDOWS USER]\\Downloads", "images_directory":"C:\\Users\\[WINDOWS USER]\\Pictures", "videos_directory":"C:\\Users\\[WINDOWS USER]\\Videos", "screenshots_directory":"C:\\Users\\[WINDOWS USER]\\Pictures\\Screenshots",  "documents_directory":"C:\\Users\\[WINDOWS USER]\\Documents"}
+EXAMPLE_DEST = {"monitored_directory":"C:\\Users\\[WINDOWS USER]\\Downloads", 
+                "images_directory":"C:\\Users\\[WINDOWS USER]\\Pictures", 
+                "videos_directory":"C:\\Users\\[WINDOWS USER]\\Videos",
+                "screenshots_directory":"C:\\Users\\[WINDOWS USER]\\Pictures\\Screenshots",
+                "documents_directory":"C:\\Users\\[WINDOWS USER]\\Documents"}
 
 def get_inputs():
     src, img, vid, ss, doc = False, False, False, False, False
@@ -50,12 +54,12 @@ def check_for_validity():
     paths = get_inputs()
     act_paths = [path for path in paths if path != False]
 
-    if validity == True and len(set(paths)) != len(act_paths):
+    if validity == True and len(set(act_paths)) == len(act_paths):
         w = Watcher(paths, window)
         w.run()
     elif validity == False:
-        messagebox.showerror('Python Error', 'Error: Paths given were not correct! \nPlease run the application again with correct paths')
-    elif len(set(paths)) != len(act_paths):
+        messagebox.showerror('Python Error', 'Error: One or more of the paths given were not correct! \nPlease run the application again with correct paths')
+    elif len(set(act_paths)) != len(act_paths):
         messagebox.showerror('Python Error', 'Error: One of the paths given occured multiple times! \nPlease run the application again with correct paths')
     else:
         messagebox.showerror('Python Error', 'Error: Unexpected error occured. \nPlease run the application again') 
@@ -73,41 +77,53 @@ screenshots_dest = StringVar()
 documents_dest = StringVar()
 
 Label(window, text="", font=("Arial 7 bold")).pack()
-Label(window, text="Directory to monitor" , font=("Arial 15 bold")).pack()
-MyEntryBox = Entry(window,textvariable = source, width=50)
-MyEntryBox.pack() 
-Button(window,text="X", cursor="hand2", command=lambda: disable_entry(MyEntryBox)).pack()
-
-Label(window, text="", font=("Arial 10 bold")).pack()
-Label(window, text="Files destinatios" , font=("Arial 15 bold")).pack()
-Label(window, text="Destination path for images").pack()
-MyEntryBox1 = Entry(window, textvariable = images_dest,width=50)
-MyEntryBox1.pack()
-Button(window,text="X", command=lambda: disable_entry(MyEntryBox1)).pack()
-
-Label(window, text="Destination path for videos").pack()
-MyEntryBox2 = Entry(window, textvariable = videos_dest, width=50)
-MyEntryBox2.pack()
-Button(window,text="X", command=lambda: disable_entry(MyEntryBox2)).pack()
 
 
-Label(window, text="Destination path for screenshots").pack()
-MyEntryBox3 = Entry(window, textvariable = screenshots_dest, width=50)
-MyEntryBox3.pack()
-Button(window,text="X", command=lambda: disable_entry(MyEntryBox3)).pack()
+frame = Frame(window)
+frame.pack(pady = 10, padx = 10)
+Label(frame, text="Directory to monitor" , font=("Arial 15 bold")).grid(row = 0, column = 0, columnspan=2)
+
+MyEntryBox = Entry(frame,textvariable = source, width=50)
+MyEntryBox.grid(row = 1, column = 0, padx = 5, pady = 5)
+Button(frame,text="X", cursor="hand2", command=lambda: disable_entry(MyEntryBox)).grid(row = 1, column = 1, padx = 5, pady = 5)
 
 
+frame1 = Frame(window)
+frame1.pack(pady = 10, padx = 10)
 
-Label(window, text="Destination path for text files").pack()
-MyEntryBox4 = Entry(window, textvariable = documents_dest, width=50)
-MyEntryBox4.pack()
-Button(window,text="X", cursor="hand2", command=lambda: disable_entry(MyEntryBox4)).pack()
+Label(frame1, text="Files destinations" , font=("Arial 15 bold")).grid(row = 0, column = 0, columnspan=2)
 
-MyEntryBox.insert(0, "C:\\Users\\[WINDOWS USER]\\Downloads")
-MyEntryBox1.insert(0, "C:\\Users\\[WINDOWS USER]\\Pictures")
-MyEntryBox2.insert(0, "C:\\Users\\[WINDOWS USER]\\Videos")
-MyEntryBox3.insert(0, "C:\\Users\\[WINDOWS USER]\\Pictures\\Screenshots")
-MyEntryBox4.insert(0, "C:\\Users\\[WINDOWS USER]\\Documents")
+Label(frame1, text="Destination path for images").grid(row = 1, column = 0, columnspan=2)
+
+MyEntryBox1 = Entry(frame1, textvariable = images_dest,width=50)
+MyEntryBox1.grid(row = 2, column = 0, padx = 5, pady = 5)
+Button(frame1,text="X", command=lambda: disable_entry(MyEntryBox1)).grid(row = 2, column = 1, padx = 5, pady = 5)
+
+
+Label(frame1, text="Destination path for videos").grid(row = 3, column = 0, columnspan=2)
+
+MyEntryBox2 = Entry(frame1, textvariable = videos_dest, width=50)
+MyEntryBox2.grid(row = 4, column = 0, padx = 5, pady = 5)
+Button(frame1,text="X", command=lambda: disable_entry(MyEntryBox2)).grid(row = 4, column = 1, padx = 5, pady = 5)
+
+Label(frame1, text="Destination path for screenshots").grid(row = 5, column = 0, columnspan=2)
+
+MyEntryBox3 = Entry(frame1, textvariable = screenshots_dest, width=50)
+MyEntryBox3.grid(row = 6, column = 0, padx = 5, pady = 5)
+Button(frame1,text="X", command=lambda: disable_entry(MyEntryBox3)).grid(row = 6, column = 1, padx = 5, pady = 5)
+
+Label(frame1, text="Destination path for screenshots").grid(row = 7, column = 0, columnspan=2)
+
+MyEntryBox4 = Entry(frame1, textvariable = documents_dest, width=50)
+MyEntryBox4.grid(row = 8, column = 0, padx = 5, pady = 5)
+Button(frame1,text="X", command=lambda: disable_entry(MyEntryBox4)).grid(row = 8, column = 1, padx = 5, pady = 5)
+
+
+MyEntryBox.insert(0, EXAMPLE_DEST["monitored_directory"])
+MyEntryBox1.insert(0, EXAMPLE_DEST["images_directory"])
+MyEntryBox2.insert(0, EXAMPLE_DEST["videos_directory"])
+MyEntryBox3.insert(0, EXAMPLE_DEST["screenshots_directory"])
+MyEntryBox4.insert(0, EXAMPLE_DEST["documents_directory"])
 
 
 Label(window, text="", font=("Arial 7 bold")).pack()
